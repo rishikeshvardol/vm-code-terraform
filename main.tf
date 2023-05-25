5618fd7d29ce608aab178ba3aa1a05221c440b45
Your Name <vry712@gmail.com>
2023-05-25 13:34:06 +0530

    added

---
+++ main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "2.90.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  # Configuration options
+
+}
+
+terraform {
+  backend "azurerm" {
+   
+  }
+}
+
